7560678e5195f079b67e23ba2a96dd1ec8d829dd
Thomas Farias <thomasfarias19@gmail.com>
2024-03-12 19:53:00 -0300

    Dashboard and orders page finalized

---
+++ src/routes.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
+import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
+import { Orders } from './pages/app/orders/orders'
+import { Dashboard } from './pages/app/dashboard/dashboard'
+import { NotFound } from './pages/404'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <NotFound />,
+    children: [
+      { path: '/', element: <Dashboard /> },
+      { path: '/orders', element: <Orders /> }
+    ],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> }, { path: '/sign-up', element: <SignUp /> }],
+  },
+])
